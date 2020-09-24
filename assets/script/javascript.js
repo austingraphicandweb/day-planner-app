b5593c65fd953aaafbd632e2df49890567a98245
Austin Keener <austingraphicandweb@gmail.com>
2020-09-24 08:59:22 -0400

    fixing the local storage save feature

--- assets/script/javascript.js
+++ assets/script/javascript.js
@@ -2,15 +2,15 @@
  * Day Planner Application
  */
 
-$(document).ready(function(){
+$(document).ready(function () {
 
- // Global Variables
+    // Global Variables
 
- // Function definition
+    // Function definition
 
     //--------------------
-        // time blocks color coded to display past, present, and future
-    function colorCode(){
+    // time blocks color coded to display past, present, and future
+    function colorCode() {
         //if time block is .block-past then display grey.
         //if time block is .block-present then display red.
         //if time block is .block-future then display green.
@@ -18,8 +18,8 @@ $(document).ready(function(){
         var blockPresent = $(".block-present");
         var blockFuture = $(".block-future");
         if (blockPast) {
-            $(blockPast).css("background-color","#ece8e2");
-        } 
+            $(blockPast).css("background-color", "#ece8e2");
+        }
         if (blockPresent) {
             $(blockPresent).css("background-color", "#e32928");
         }
@@ -27,20 +27,20 @@ $(document).ready(function(){
             $(blockFuture).css("background-color", "#9df200");
         }
     }
-    
+
     //--------------------
     // Header display 
     //function displayDay is going to show the current month, day, and year in the jumbotron.
     function displayDay() {
         $(".lead").append(moment().format('dddd MMMM Do YYYY'));
-        $(".lead").css("margin","60px 0px 0px 0px");
+        $(".lead").css("margin", "60px 0px 0px 0px");
         $(".lead").css("font-size", "25px")
     }
     //-------------------
 
     // Time display on the left hand side of the page. This indicates which block will save information for what hour within a standard work day.
     function time() {
-        $(".style").css("margin","10px");
+        $(".style").css("margin", "10px");
         $(".block-1").append(textContent = "9 a.m.");
         $(".block-2").append(textContent = "10 a.m.");
         $(".block-3").append(textContent = "11 a.m.");
@@ -54,45 +54,69 @@ $(document).ready(function(){
     }
     //------------------
 
- // Function calls
- displayDay();
- time();
- colorCode();
- // Event listeners
+    // Function calls
+    colorCode();
+    displayDay();
+    time();
+    // Event listeners
 
     // -----------------------------------------
     // Save button 
     // I have given each input a unique class so that when looking at the saved content within localStorage you can see the most recent save for each hour throughout the standard work day.
-$(".container").on("click", ".btn", function (event){
-    event.preventDefault(); 
-    var content = $(".content-0").val();
-    var array = JSON.stringify(content);
-    localStorage.setItem("content-0",array);
-    var content = $(".content-1").val();
-    var array = JSON.stringify(content);
-    localStorage.setItem("content-1",array);
-    var content = $(".content-2").val();
-    var array = JSON.stringify(content);
-    localStorage.setItem("content-2",array);
-    var content = $(".content-3").val();
-    var array = JSON.stringify(content);
-    localStorage.setItem("content-3",array);
-    var content = $(".content-4").val();
-    var array = JSON.stringify(content);
-    localStorage.setItem("content-4",array);
-    var content = $(".content-5").val();
-    var array = JSON.stringify(content);
-    localStorage.setItem("content-5",array);
-    var content = $(".content-6").val();
-    var array = JSON.stringify(content);
-    localStorage.setItem("content-6",array);
-    var content = $(".content-7").val();
-    var array = JSON.stringify(content);
-    localStorage.setItem("content-7",array);
-    var content = $(".content-8").val();
-    var array = JSON.stringify(content);
-    localStorage.setItem("content-8",array);
-})
-// -----------------------------------------
+    $(".rowOne").on("click", ".btn", function (event) {
+        event.preventDefault();
+        var content = $(".content-0").val();
+        var array = JSON.stringify(content);
+        localStorage.setItem("content-0", array);
+    });
+    $(".rowTwo").on('click', '.btn', function (event) {
+        event.preventDefault();
+        var content = $(".content-1").val();
+        var array = JSON.stringify(content);
+        localStorage.setItem("content-1", array);
+    })
+    $(".rowThree").on('click', '.btn', function (event) {
+        event.preventDefault();
+        var content = $(".content-2").val();
+        var array = JSON.stringify(content);
+        localStorage.setItem("content-2", array);
+    })
+    $(".rowFour").on('click', '.btn', function (event) {
+        event.preventDefault();
+        var content = $(".content-3").val();
+        var array = JSON.stringify(content);
+        localStorage.setItem("content-3", array);
+    })
+    $(".rowFive").on('click', '.btn', function (event) {
+        event.preventDefault();
+        var content = $(".content-4").val();
+        var array = JSON.stringify(content);
+        localStorage.setItem("content-4", array);
+    })
+    $(".rowSix").on('click', '.btn', function (event) {
+        event.preventDefault();
+        var content = $(".content-5").val();
+        var array = JSON.stringify(content);
+        localStorage.setItem("content-5", array);
+    })
+    $(".rowSeven").on('click', '.btn', function (event) {
+        event.preventDefault();
+        var content = $(".content-6").val();
+        var array = JSON.stringify(content);
+        localStorage.setItem("content-6", array);
+    })
+    $(".rowEight").on('click', '.btn', function (event) {
+        event.preventDefault();
+        var content = $(".content-7").val();
+        var array = JSON.stringify(content);
+        localStorage.setItem("content-7", array);
+    })
+    $(".rowNine").on('click', '.btn', function (event) {
+        event.preventDefault();
+        var content = $(".content-8").val();
+        var array = JSON.stringify(content);
+        localStorage.setItem("content-8", array);
+    })
+    // -----------------------------------------
 
 }) // this is the end of the very first part of my script which is $(document).ready(function{}) which is what determines if my code is ready to run or not.
